99e730d8c2402c6e2c82b3b06787f752818c6af0
prinses-racwa <rajwaamelia@gmail.com>
2026-05-28 11:22:20 +0700

    rampung.

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -23,7 +23,7 @@ public:
         cout << endl;
 
         //cek apakah antrian penuh
-        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR = 1))
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT = REAR = 1))
         {
             cout << "\nQueues overflow\n";
             return;
